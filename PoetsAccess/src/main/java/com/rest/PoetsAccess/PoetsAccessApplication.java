fc896d7b026c6ec3deaa47fa0bfbb5d8aa6be7b3
JWSTIEMANN <jwstiemann@gmail.com>
2023-02-08 11:49:11 -0500

    Initial commit.

---
+++ PoetsAccess/src/main/java/com/rest/PoetsAccess/PoetsAccessApplication.java
@@ -0,0 +1,13 @@
+package com.rest.PoetsAccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetsAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetsAccessApplication.class, args);
+	}
+
+}
---
+++ PoetsAccess/src/test/java/com/rest/PoetsAccess/PoetsAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.PoetsAccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetsAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
